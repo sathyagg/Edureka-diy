f03bb570ed18840555a0798ca751e1008eaab76d
Sathya <159009564+sathyagg@users.noreply.github.com>
2024-02-05 15:03:58 +0530

    Add files via upload

---
+++ sathya javascript/javascript.js
@@ -0,0 +1,3 @@
+console.log(5,10,15,20);
+console.log("hello world");
+console.log("true");
---
+++ sathya javascript/samplejavascript.js
@@ -0,0 +1,76 @@
+//variable day-10
+//alerting
+alert("hello my name is Name,i'm learing javacsript");
+
+//logging
+console.log("hello my name is Name,i'm learing javacsript");
+//shortcut 1
+var meg = "hello my name is Name,i'm learing javacsript";
+ //alerting
+ alert(msg)
+ //logging
+ console.log(msg)
+ //shortcut 2
+ var name = "sathya"
+ var msg = "hello my name is "  + name + "i'm learing javacsript";
+ //alert
+alert(msg)
+//logging
+console.log(msg);
+//addvance features
+let number1 = 5;
+let number2 = 45;
+let result = number1 + number2;
+console.log(result);
+//rules
+//keywort(var,if,let,constant)
+// no javascript keywords name;let if
+//should not start with number;1number
+//fullname eppadi tharakutathu and (fullName) eppadi tharaventum);1 word 1 letter small,2word1letter capital
+//(full name) space vittu tharakutathu.(fullname eppadi tharaventum);space
+//(FullName)eppadi tharakutathu;case
+//meaningfull names
+//constants day11
+const name = "sathya";
+const dob = "09-05-2000";
+let msg = "hello my name is" + name + "i born on " + dob + "i'm learnig in javascript";
+//data type day12
+let firstname = "sathya";// string
+let age = 23;//integer
+let isyoung = true//boolen
+let lastname = undefined;//undefined
+let tyophy = null//null
+ //dynamic typing day13
+      //let firstname = "sathya";// string//output console ( typeof firstName-string)varum/console(lastname-sathya varum)
+     //let age = 23;//integer
+     //let isyoung = true//boolen
+    //let lastname = undefined;//undefined
+    //let tyophy = null//null
+    //object {} ,day14
+    let person = {
+        name: "sathya",
+        age: 23,
+        gender: "female",
+        address: "salem, tamilnadu,india"
+        sibling: {
+            brother: "manivel kannan",
+            sister: "anitha kaviya"
+        }
+    }
+    person.age = 24
+
+    //dot notation(.)ex
+    console.log(person.sibling.sister)//(person.name,age,genter,address)is called
+    //bracket notation
+      //console.log(person['age'])//(person['name'],'age','genter','address')is called
+      //arrays day-15[]square bracket
+      let favcolors = ["red","blue","black"]
+          favcolors[4] = sathya
+          //[]
+          console.log(favcolors.length)//red,blue,black,emedy,sathya its lengath
+        
+
+    }
+
+
+
