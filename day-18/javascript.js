9cc9c0df5fcc702ae84715fd3abb3f260d4f8157
Sathya <159009564+sathyagg@users.noreply.github.com>
2024-02-09 13:37:15 +0530

    Add files via upload

---
+++ day-18/javascript.js
@@ -0,0 +1,35 @@
+//arithimetic operators
+var number1 = 5;
+var number2 = 10;
+        console.log(number1+ number2);
+        console.log(number1-number2);
+        console.log(number1* number2);
+        console.log(number1/ number2);
+        console.log(number1% number2);
+x = "hellow"; y = "world";
+console.log(x==y);
+y= "hellow";
+console.log(x==y);
+x=23; y='23'
+console.log(x==y);
+console.log(x===y);
+console.log(x!==y);
+y = 23;
+console.log(x===y);
+console.log(x<y);
+console.log(x>y);
+console.log(x<=y);
+console.log(x>=y);
+console.log(x!=y);
+x = 123; y = 456;
+console.log(x,y);
+x+=5;
+console.log(x);
+console.log(x&&y);
+console.log(x||y);
+console.log(x=== y ?"hellow" : 'world');
+
+
+
+
+ 
