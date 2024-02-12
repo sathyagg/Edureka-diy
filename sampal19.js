a42ffdb9bb2ffbd0e130938a55fcfd2988c107c1
Sathya <159009564+sathyagg@users.noreply.github.com>
2024-02-12 15:56:31 +0530

    Add files via upload

---
+++ sampal19.js
@@ -0,0 +1,56 @@
+// let i = 1;
+// while (i<=50) {
+//   // console.log(i)
+//   i=i+2;
+
+//   }
+
+//   let a = 50;
+//   do{
+//     // console.log(a);
+//     i=i+1;
+//   }
+//   while(i<50)
+
+//   // 23456789
+//   for(i=1;i<=10;i++){
+//     console.log(i);
+//   }
+//for loops--------
+// const array = [1,2,3,4,5,6,7,8,9];
+// for (let index = 0; index < array.length; index++) {
+  // console.log("value at intex:"+index+"is:"+array[2]);
+// }
+//while loops-----------
+// let index =10;
+// while (index>0) {
+//   // console.log(index);
+//   index--;
+// }
+//do while loops-----------------
+// let num = 20;
+// do{
+//   console.log(num);
+//   num--;
+// }
+// while(num>0)
+//for in loops---------------------
+// var obj = {
+//   name : "sathya",
+//   course : "full stock wep development",
+//   duration: 4,
+// }
+// for (let key in obj) {
+//     console.log("key is:"+key);
+//     console.log(obj[key]);
+    
+//   }
+//for of loops--------------
+const array=[1,2,3,4,5,6,7,8,9];
+for(i of array){
+  console.log(i);
+}
+for (let i = 5; i>=1; i++) {
+  console.log("welcome to edureka" +i);
+  
+}
