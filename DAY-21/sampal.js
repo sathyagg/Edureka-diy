ac8ddcb79e34eaa373f8be2d791d91fb434f3a07
Sathya <159009564+sathyagg@users.noreply.github.com>
2024-02-17 15:53:56 +0530

    Add files via upload

---
+++ DAY-21/sampal.js
@@ -0,0 +1,19 @@
+// let a = [1,2,3,4,5,6,7,];
+// console.log(a);
+// console.table(a);
+// console.log(a[1]);
+
+// let b =new Array(1,2,3,4,5,6,7);
+// console.table(b)
+// let c = new Array("sathya",30,true,{m1:100,m2:200,m3:300})
+// console.table(c);
+// const number = [1,2,30,3,4,5,6,7];
+// number.forEach((value)=>{
+//     console.log(value);
+// });
+const arr =[10,20,30,40];
+document.write(<h1> "intaily:" + arr </h1>);
+arr.push(100,120);
+document.write(<h1> "push in end:" + arr </h1>);
+arr.unshift(1,2,3);
+document.write(<h1>"push is the first: " + arr </h1>);
